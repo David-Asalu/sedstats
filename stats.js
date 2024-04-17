2578320ebacc6c6f81443d9d69c6a2d9c742d3d2
David Asalu <106029295+David-Asalu@users.noreply.github.com>
2024-04-17 13:16:36 +0100

    Add files via upload

---
+++ stats.js
@@ -0,0 +1,79 @@
+
+function showSelectedForm() {
+    var selectedCalculation = document.getElementById('calculationType').value;
+    var formContainer = document.getElementById('form-container');
+    formContainer.innerHTML = '';
+
+    if (selectedCalculation === 'skewness') {
+        formContainer.innerHTML = `
+            <div class="input-group">
+                <label for="sixteenphi">Sixteen Phi:</label>
+                <input type="number" id="sixteenphi" class="input" placeholder="Enter sixteen phi">
+            </div>
+            <div class="input-group">
+                <label for="eightyfourphi">Eighty-four Phi:</label>
+                <input type="number" id="eightyfourphi" class="input" placeholder="Enter eighty-four phi">
+            </div>
+            <div class="input-group">
+                <label for="fiftyphi">Fifty Phi:</label>
+                <input type="number" id="fiftyphi" class="input" placeholder="Enter fifty phi">
+            </div>
+            <div class="input-group">
+                <label for="ninetyfivephi">Ninety-five Phi:</label>
+                <input type="number" id="ninetyfivephi" class="input" placeholder="Enter ninety-five phi">
+            </div>
+            <div class="input-group">
+                <label for="fivephi">Five Phi:</label>
+                <input type="number" id="fivephi" class="input" placeholder="Enter five phi">
+            </div>
+            <button onclick="calculateSkewness()">Calculate Skewness</button>
+        `;
+    } else if (selectedCalculation === 'kurtosis') {
+      
+        formContainer.innerHTML = `
+            <div class="input-group">
+                <label for="twentyfivephi">Twenty-five Phi:</label>
+                <input type="number" id="twentyfivephi" class="input" placeholder="Enter twenty-five phi">
+            </div>
+            <div class="input-group">
+                <label for="seventyfivephi">Seventy-five Phi:</label>
+                <input type="number" id="seventyfivephi" class="input" placeholder="Enter seventy-five phi">
+            </div>
+            <div class="input-group">
+            <label for="ninetyfivephi">Ninety-five Phi:</label>
+            <input type="number" id="ninetyfivephi" class="input" placeholder="Enter ninety-five phi">
+        </div>
+        <div class="input-group">
+            <label for="fivephi">Five Phi:</label>
+            <input type="number" id="fivephi" class="input" placeholder="Enter five phi">
+        </div>
+            <button onclick="calculateKurtosis()">Calculate Kurtosis</button>
+            
+        `;
+        document.getElementById('result').innerText = " " ;
+    }
+}
+
+function calculateSkewness() {
+       
+    var sixteen_phi = parseFloat(document.getElementById('sixteenphi').value);
+var eightyfour_phi = parseFloat(document.getElementById('eightyfourphi').value);
+var fifty_phi = parseFloat(document.getElementById('fiftyphi').value);
+var ninetyfive_phi = parseFloat(document.getElementById('ninetyfivephi').value);
+var five_phi = parseFloat(document.getElementById('fivephi').value);
+    var skewness = ((((sixteen_phi + eightyfour_phi) - (2 * fifty_phi)) / (2 * (eightyfour_phi - sixteen_phi))) + (((five_phi + ninetyfive_phi) - (2 * fifty_phi)) / (2 * (ninetyfive_phi - five_phi))));
+
+    document.getElementById('result').innerText = "Skewness: " + skewness;
+}
+function calculateKurtosis() {
+    var ninetyfive_phi = parseFloat(document.getElementById('ninetyfivephi').value);
+    var five_phi = parseFloat(document.getElementById('fivephi').value);
+    var seventyfive_phi= parseFloat(document.getElementById('seventyfivephi').value);
+    var twentyfive_phi= parseFloat(document.getElementById('twentyfivephi').value);
+    var kurtosis = ((ninetyfive_phi - five_phi) / (2.44 * (seventyfive_phi - twentyfive_phi)));
+
+    document.getElementById('result').innerText = "Kurtosis: " + kurtosis;
+}
+
+// Show Skewness form by default
+showSelectedForm();
